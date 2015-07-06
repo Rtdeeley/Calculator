b7f36e9b94e5e617f582123b3797126d0f2d152c
Rtdeeley <rtdeeley1335@gmail.com>
2015-07-06 17:01:12 -0500

    Namespace Edits

---
+++ Rob.Calc.BLL/MathWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rob.Calc.BLL
+{
+    public class MathWorker
+    {
+        public double AddResult(double num1, double num2)
+        {
+            return (num1 + num2);
+        }
+
+        public double SubResult(double num1, double num2)
+        {
+            return (num1 - num2);
+        }
+
+        public double MultResult(double num1, double num2)
+        {
+            return (num1 * num2);
+        }
+
+        public double DivResult(double num1, double num2)
+        {
+            return (num1 / num2);
+        }
+    }
+}
